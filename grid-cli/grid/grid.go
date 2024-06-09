2acc902d684206a071cfc44b35f54a7f3bafb5af
Steve Traugott <stevegt@t7a.org>
2024-06-08 20:45:49 -0700

    Refactor grid.go, shift functions to KernelNative with enhancements
    Remove two imports and two functions from grid.go
    
    summary of diff --git  a/grid-cli/grid/grid.go b/grid-cli/grid/grid.go
    
    - Remove "path/filepath" from import
    - Comment out the import of "github.com/stevegt/goadapt"
    - Remove the entire `executeSubcommand` function
    - Remove the `fetchLocalData` function
    
    Add execution and data fetching capabilities to KernelNative
    
    summary of diff --git  a/grid-cli/grid/kernelNative.go b/grid-cli/grid/kernelNative.go
    
    - Add `os/exec` package to imports
    - Add `github.com/stevegt/goadapt` package to imports with dot import notation
    - Implement `executeSubcommand` method in `KernelNative` struct to run subcommands using the fetched module
    - Direct stdout, stderr, and stdin of the subcommand to the respective os counterparts
    - Exit with status 1 on error executing subcommand and print the error
    - Implement `fetchLocalData` method in `KernelNative` struct to fetch data from local cache
    - Return an error if data not found in local cache
    - Use formatted cache path to read file in `fetchLocalData`
    - Include placeholder comment for extending functionality to check known handler if data not found in cache

--- grid-cli/grid/grid.go
+++ grid-cli/grid/grid.go
@@ -4,12 +4,11 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 	"sync"
 
 	"github.com/gorilla/websocket"
-	. "github.com/stevegt/goadapt"
+	// . "github.com/stevegt/goadapt"
 )
 
 const (
@@ -56,35 +55,4 @@ func showPromise(sys *KernelNative, subcommand string) {
 		os.Exit(1)
 	}
 	fmt.Println(string(output))
-}
-
-func (sys *KernelNative) executeSubcommand(subcommand string, args []string) {
-	symbolTableHash, err := sys.getSymbolTableHash()
-	Ck(err)
-	symbolTable := fetchSymbolTable(symbolTableHash)
-	subcommandHash := getSubcommandHash(symbolTable, subcommand)
-	module := sys.fetchModule(subcommandHash)
-	cmd := exec.Command(module, args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-	if err := cmd.Run(); err != nil {
-		fmt.Println("Error executing subcommand:", err)
-		os.Exit(1)
-	}
-}
-
-func (sys *KernelNative) fetchLocalData(mBuf []byte) ([]byte, error) {
-	fn := fmt.Sprintf("%x", mBuf)
-	cachePath := filepath.Join(sys.baseDir, cacheDir, fn)
-	data, err := sys.util.ReadFile(cachePath)
-	if err == nil {
-		return data, nil
-	}
-
-	// XXX If data not found in cache, check if it's a known handler
-	// handlerPath := filepath.Join(os.Getenv("HOME"), gridDir, "handlers", hash)
-	// return ioutil.ReadFile(handlerPath)
-
-	return nil, fmt.Errorf("Data not found.")
 }
